e155a4cc4cd81e809129ea29a745de4b0b5fa67a
Wes Cossick <wes@nextstepwebs.com>
2015-09-27 10:46:53 -0500

    fix

--- approve_direct.php
+++ approve_direct.php
@@ -3,7 +3,7 @@ require($_SERVER['DOCUMENT_ROOT'].'/config/main.php');
 
 $suggestions = suggested_times([$_POST['user_email']]);
 
-confirm_request($_GET['id']);
+confirm_request($_POST['id']);
 
 echo json_encode($suggestions, true);
 ?>
--- approve_email.php
+++ approve_email.php
@@ -3,7 +3,7 @@ require($_SERVER['DOCUMENT_ROOT'].'/config/main.php');
 
 $suggestions = suggested_times([$_POST['user_email']]);
 
-reply_request($_GET['id']);
+reply_request($_POST['id']);
 
 approve_meeting($_POST['sender_email'], $_POST['user_email'], $_POST['subject'], $suggestions);
 ?>
