8d90443bfae2918865383c5fab9861f1b9b12b53
Evan-Green <evan@Evans-MacBook-Pro.local>
2015-09-27 09:04:59 -0500

    added the coach

--- calendar_test.php
+++ calendar_test.php
@@ -1,15 +1,8 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'].'/config/main.php');
 
-$events = get_all_events('grilled.moose@gmail.com');
 
-$free_time = free_time('grilled.moose@gmail.com');
-print 'Free time:<br>';
-var_dump($free_time);
-print '<br><br>Events:<br>';
-var_dump($events);
-
-$out = suggested_times(['grilled.moose@gmail.com']);
+$out = suggested_times(['kenstarr1845@gmail.com', 'artbriles1845@gmail.com']);
 print_r($out);
 
 ?>
