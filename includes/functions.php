60892688715f2d7943f8e041cb9dc3314ac5dfaa
Evan-Green <evan@Evans-MacBook-Pro.local>
2015-09-27 07:13:24 -0500

    more test

--- includes/functions.php
+++ includes/functions.php
@@ -141,9 +141,9 @@ function days_gone_by($recipient)
     {
         global $PDO;
         
-        $current = date('Y-m-d', (time() + (($i*(-1)-1) * 24 * 60 * 60)));
+        $current = date('Y-m-d H-i-s', (time() + (($i*(-1)-1) * 24 * 60 * 60)));
         $yesterday = $nextWeek = time() + (($i*(-1)) * 24 * 60 * 60);
-        $past = date('Y-m-d', $yesterday);
+        $past = date('Y-m-d H-i-s', $yesterday);
         
         $current_date = date('l', strtotime('-'.$i.'days'));
         $query = 'SELECT COUNT(*) FROM meeting_requests WHERE recipient = :email AND date_received >= :past AND date_received <= :current';
