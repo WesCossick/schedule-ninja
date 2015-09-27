c363f346ed4aaf85611250a57189bb01b91b4081
Samuel Taylor <github@samueltaylor.org>
2015-09-27 01:23:39 -0500

    Add data generator

--- test.php
+++ test.php
@@ -1,5 +1,19 @@
 <?php require('config/main.php'); ?>
 
 <?php
-echo count_meeting_requests('grilled.moose@gmail.com');
+for (var $i = 0; $i < 10; $i++) {
+    $type = $i % 2 == 0 ? 'direct' : 'email';
+
+    $int = mt_rand(1440646225,1443334681);
+    $date_received = date("Y-m-d H:i:s", $int);
+    $recipient = $i % 2 == 0 ? 'grilled.moose@gmail.com' : 'htx@samueltaylor.org';
+    $sender_name = '';
+    $sender_email = "$sender_name@samueltaylor.org";
+
+    $constraints_after = $i % 4 == 0 ? NULL : date("Y-m-d H:i:s", $int);
+    $constraints_before = $i % 3 == 0 ? NULL : date("Y-m-d H:i:s", $int + 60 * 60 * 24 * 7);
+    $requested_date = NULL;
+
+    create_meeting_request($type, $date_received, $recipient, $sender_email, $sender_name, $constraints_after, $constraints_before, $requested_date) {
+}
 ?>
