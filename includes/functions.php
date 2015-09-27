a1fcbab5f36120d88f51a47cbdf7c09df8f4621a
Wes Cossick <wes@nextstepwebs.com>
2015-09-27 00:08:45 -0500

    Add example code

--- includes/functions.php
+++ includes/functions.php
@@ -2,5 +2,16 @@
 function meeting_requests()
 {
     // Do something
+    $query = "SELECT * FROM users WHERE email = :email";
+    $statement = $PDO->prepare($query);
+    $params = array(
+        'email' => 'something@example.com',
+    );
+    $statement->execute($params);
+    $row = $statement->fetch();
+    
+    
+    // Return
+    return $row;
 }
 ?>
