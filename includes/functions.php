a0638a65ae3160ef2681564698d4ac0d258fff22
Evan-Green <evan@Evans-MacBook-Pro.local>
2015-09-27 07:33:28 -0500

    evan was right

--- includes/functions.php
+++ includes/functions.php
@@ -141,7 +141,7 @@ function days_gone_by($recipient)
     {
         global $PDO;
         
-        $current = date('Y-m-d H:i:s', strtotime(date('Y-m-d', strtotime('-'.($i+1).' days'))));
+        $current = date('Y-m-d H:i:s', strtotime(date('Y-m-d', strtotime('-'.($i-1).' days'))));
         $past = date('Y-m-d H:i:s', strtotime(date('Y-m-d', strtotime('-'.$i.' days'))));
         
         print_r($past);
