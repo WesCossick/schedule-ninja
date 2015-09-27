0ac17fd544980bdf6a1e53f18d3116a2a6ca0afe
Samuel Taylor <github@samueltaylor.org>
2015-09-27 06:48:01 -0500

    Add more test

--- calendar_test.php
+++ calendar_test.php
@@ -2,6 +2,11 @@
 require($_SERVER['DOCUMENT_ROOT'].'/config/main.php');
 
 $events = get_all_events('grilled.moose@gmail.com');
+
 $free_time = free_time($events);
+print 'Free time:<br>';
 var_dump($free_time);
+print '<br><br>Events:<br>';
+var_dump($events);
+
 ?>
