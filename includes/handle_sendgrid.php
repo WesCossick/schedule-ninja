ddaef81130c1bedc5c33d9436b827304fd44673b
Evan-Green <evan@Evans-MacBook-Pro.local>
2015-09-27 04:27:40 -0500

    EOL

--- includes/handle_sendgrid.php
+++ includes/handle_sendgrid.php
@@ -8,9 +8,9 @@ function reject_meeting($to_address, $from_address, $original_subject)
 {
     $name = get_name_of_user($from_address);
     $subject = 'RE:'.$original_subject;
-    $text = 'Hello,\nI am '.$name.'\'s personal Schedule Ninja! '
+    $text = 'Hello,'.PHP_EOL.'I am '.$name.'\'s personal Schedule Ninja! '
             .$name.' got your meeting request, but unfortunately cannot meet with you. 
-			We apologize for any inconvenience!\nThanks!\nSchedule Ninja';
+			We apologize for any inconvenience!'.PHP_EOL.'Thanks!'.PHP_EOL.'Schedule Ninja';
     $html = '<strong>'.$text.'</strong>';
     
     send_email($to_address, $subject, $text, $html);
@@ -20,9 +20,9 @@ function send_meeting($to_address, $from_address)
 {
     $name = get_name_of_user($from_address);
     $subject = $name.' would like to schedule a meeting!';
-    $text = 'Hello,\nI am '.$name.'\'s personal Schedule Ninja! '
+    $text = 'Hello,'.PHP_EOL.'I am '.$name.'\'s personal Schedule Ninja! '
             .$name.' has requested that I set up a meeting with you.
-            Are you free from blank to blank?\nThanks!\nSchedule Ninja';
+            Are you free from blank to blank?'.PHP_EOL.'Thanks!'.PHP_EOL.'Schedule Ninja';
     $html = '<strong>'.$text.'</strong>';
     
     send_email($to_address, $subject, $text, $html);
@@ -32,9 +32,9 @@ function approve_meeting($to_address, $from_address, $original_subject)
 {
     $name = get_name_of_user($from_address);
     $subject = 'RE:'.$original_subject;
-    $text = 'Hello,\nI am '.$name.'\'s personal Schedule Ninja! '
+    $text = 'Hello,'.PHP_EOL.'I am '.$name.'\'s personal Schedule Ninja! '
             .$name.' got your meeting request, and would love to schedule a time
-             to meet. Are you free from blank to blank?\nThanks!\nSchedule Ninja';
+             to meet. Are you free from blank to blank?'.PHP_EOL.'Thanks!'.PHP_EOL.'Schedule Ninja';
     $html = '<strong>'.$text.'</strong>';
     
     send_email($to_address, $subject, $text, $html);
@@ -44,9 +44,6 @@ function send_email($to_address, $subject, $text, $hmtl)
 {
     global $SENDGRID_USER, $SENDGRID_PASS;
     
-	print_r($SENDGRID_USER);
-	print_r($SENDGRID_PASS);
-	
     $sendgrid = new SendGrid($SENDGRID_USER, $SENDGRID_PASS);
     $email    = new SendGrid\Email();
     $email->addTo($to_address)->
@@ -55,8 +52,7 @@ function send_email($to_address, $subject, $text, $hmtl)
         setText($text)->
         setHtml($html);
     
-    $blacky = $sendgrid->send($email);
-	print_r($blacky);
+    $sendgrid->send($email);
 }
 
 function get_name_of_user($from_address)
