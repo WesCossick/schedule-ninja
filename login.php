1fb31b591d18ea4d0791e82ed9f0eb9542f80c7f
Wes Cossick <wes@nextstepwebs.com>
2015-09-27 00:26:36 -0500

    Add login page

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SB Admin - Bootstrap Admin Template</title>
+    <?php require($_SERVER['DOCUMENT_ROOT'].'/includes/head.php'); ?>
+</head>
+
+<body>
+    <div id="wrapper">
+        <?php require($_SERVER['DOCUMENT_ROOT'].'/includes/navigation.php'); ?>
+
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Login <small>Welcome!</small>
+                        </h1>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        Test
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require($_SERVER['DOCUMENT_ROOT'].'/includes/scripts.php'); ?>
+</body>
+</html>
