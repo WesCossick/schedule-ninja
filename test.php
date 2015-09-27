d31e44abc4848bf502ddbce7fa477633e0dc2ae7
Samuel Taylor <github@samueltaylor.org>
2015-09-27 01:49:38 -0500

    Make test names in test

--- test.php
+++ test.php
@@ -8,6 +8,12 @@ for ($i = 0; $i < 10; $i++) {
     $date_received = date("Y-m-d H:i:s", $int);
     $recipient = $i % 2 == 0 ? 'grilled.moose@gmail.com' : 'htx@samueltaylor.org';
     $sender_name = '';
+    $characters = 'abcdefghijklmnopqrstuvwxyz';
+    $charactersLength = strlen($characters);
+    for ($i = 0; $i < 9; $i++) {
+        $sender_name .= $characters[rand(0, $charactersLength - 1)];
+    }
+                            }
     $sender_email = "$sender_name@samueltaylor.org";
 
     $constraints_after = $i % 4 == 0 ? NULL : date("Y-m-d H:i:s", $int);
