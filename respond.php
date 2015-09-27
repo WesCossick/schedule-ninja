f59d0129221f7d75621df458450b3ecd3dcdaa98
Wes Cossick <wes@nextstepwebs.com>
2015-09-27 10:53:15 -0500

    Deploy

--- respond.php
+++ respond.php
@@ -3,5 +3,5 @@ require($_SERVER['DOCUMENT_ROOT'].'/config/main.php');
 
 confirm_stuff($_GET['sender'], $_GET['recipient']);
 
-header("Location: /?success=You're scheduled for ".date('m/d/Y', $_GET['time']));
+header("Location: /?success=You're scheduled for ".date('m/d/Y', $_GET['time']).' at '.date('H:i', $_GET['time']));
 ?>
