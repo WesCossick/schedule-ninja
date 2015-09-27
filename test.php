e4f4d41d8ebfe87b854eda3776b555e99356ef67
Samuel Taylor <github@samueltaylor.org>
2015-09-27 02:53:49 -0500

    Update test.php

--- test.php
+++ test.php
@@ -18,7 +18,7 @@ for ($i = 0; $i < 10; $i++) {
     $type = $i % 2 == 0 ? 'direct' : 'email';
     $int = mt_rand(1440646225,1443334681);
     $date_received = date("Y-m-d H:i:s", $int);
-    $recipient = $i % 2 == 0 ? 'grilled.moose@gmail.com' : 'htx@samueltaylor.org';
+    $recipient = $i % 3 == 0 ? 'grilled.moose@gmail.com' : 'htx@samueltaylor.org';
     $sender_name = random_string(9, 'abcdefghijklmnopqrstuvwxyz');
     $sender_email = "$sender_name@samueltaylor.org";
     $hours = rand(1, 6);
